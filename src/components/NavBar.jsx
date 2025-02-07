ec8e5b3b4f99f879882f78b0b5dbd21549571dda
Alan <alan.pessoal2003@gmail.com>
2025-02-07 01:36:20 -0300

    Criação da estilização dos arquivos - Footer / Nav / Home / MainPage

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { NavLink, useNavigate } from "react-router-dom";
 import { useGastos } from "../context/GastosContext";
+import "./NavBar.css";
 
 function Navbar() {
   const { gastos, salario } = useGastos();
@@ -9,30 +10,27 @@ function Navbar() {
 
   return (
     <nav id="navBar">
-      <div id="icone">
-        <NavLink to="/HomePage">
-          <img
-            src="icon.png"
-            alt="simbolos relacionados a temática de orçamento"
-            width={"60px"}
-            height={"60px"}
-          />
+      <NavLink to="/HomePage">
+        <img src="logo.png" alt="Ícone da página" />
+      </NavLink>
+
+      <div id="nav-link">
+        <NavLink to="/MainPage" className="link">
+          Início
+        </NavLink>
+
+        <NavLink to="/Metas" className="link">
+          Metas
+        </NavLink>
+
+        <NavLink to="/Gastos" state={{ gastos, salario }} className="link">
+          Gastos
+        </NavLink>
+
+        <NavLink to="/Resume" state={{ gastos, salario }} className="link">
+          Gráfico
         </NavLink>
       </div>
-
-      <NavLink to="/MainPage" className="link">
-        Início
-      </NavLink>
-
-      <NavLink to="/Metas" className="link">Metas</NavLink>
-
-      <NavLink to="/Gastos" state={{ gastos, salario }} className="link">
-        Gastos
-      </NavLink>
-
-      <NavLink to="/Resume" state={{ gastos, salario }} className="link">
-        Gráfico
-      </NavLink>
     </nav>
   );
 }
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
-
 import { useGastos } from "../context/GastosContext";
+import './MainPage.css'
 
 function MainPage() {
   const { gastos, adicionarGasto, limparGastos } = useGastos();
@@ -85,20 +85,20 @@ function MainPage() {
   };
 
   return (
-    <section>
-      <div id="pagPrincipal">
+    <section id="section-main">
+    {/* Coluna Esquerda */}
+    <div className="container">
+      <div className="detalhes-usuario">
         <h1>Detalhes do Usuário</h1>
-        <div>
-          <p>Bem-vindo, {nome}!</p>
-          <p>Salário: R${salarioInput}</p>
-        </div>
+        <p>Bem-vindo, {nome}!</p>
+        <p>Salário: R$ {salarioInput}</p>
       </div>
-
-      <div id="form-gastos">
+  
+      <div className="form-gastos">
         <h2>Cadastro de Gastos</h2>
         <form onSubmit={handleSubmit}>
-          <label>
-            Nome do Gasto:
+          <div className="form-group">
+            <label>Nome do Gasto:</label>
             <input
               type="text"
               name="nome"
@@ -106,9 +106,10 @@ function MainPage() {
               onChange={handleChange}
               required
             />
-          </label>
-          <label>
-            Data do Gasto:
+          </div>
+  
+          <div className="form-group">
+            <label>Data do Gasto:</label>
             <input
               type="date"
               name="data"
@@ -116,19 +117,21 @@ function MainPage() {
               onChange={handleChange}
               required
             />
-          </label>
-          <label>
-            Tipo do Gasto:
+          </div>
+  
+          <div className="form-group">
+            <label>Tipo do Gasto:</label>
             <select name="tipo" value={gasto.tipo} onChange={handleChange}>
-              <option value="alimentação">Alimentação</option>
-              <option value="transporte">Transporte</option>
-              <option value="saúde">Saúde</option>
-              <option value="entretenimento">Entretenimento</option>
-              <option value="geral">Gastos gerais</option>
+              <option value="Alimentação">Alimentação</option>
+              <option value="Transporte">Transporte</option>
+              <option value="Saúde">Saúde</option>
+              <option value="Entretenimento">Entretenimento</option>
+              <option value="Geral">Gastos gerais</option>
             </select>
-          </label>
-          <label>
-            Valor do Gasto:
+          </div>
+  
+          <div className="form-group">
+            <label>Valor do Gasto:</label>
             <input
               type="number"
               name="valor"
@@ -136,33 +139,42 @@ function MainPage() {
               onChange={handleChange}
               required
             />
-          </label>
+          </div>
+  
           <button type="submit">Cadastrar Gasto</button>
         </form>
       </div>
-
+    </div>
+  
+    {/* Coluna Direita */}
+    <div className="section-gastos">
       <div id="lista-gastos">
         <h2>Lista de Gastos</h2>
         {gastos.length > 0 ? (
           <ul>
-            {gastos.map((gasto, index) => (
-              <li key={index}>
-                <strong>{gasto.nome}</strong> - {gasto.tipo} - R${gasto.valor} -{" "}
-                {gasto.data}
-              </li>
-            ))}
-          </ul>
+          {gastos.map((gasto, index) => (
+            <li key={index}>
+              <strong>{gasto.nome}</strong>
+              <span>{gasto.tipo}</span>
+              <p>R${gasto.valor}</p>
+              <span>{gasto.data}</span>
+            </li>
+          ))}
+        </ul>
+        
         ) : (
           <p>Não há gastos cadastrados.</p>
         )}
       </div>
-
+  
       <div id="acoes">
         <button onClick={handleGrafico}>Gerar Gráfico</button>
         <button onClick={handleVerGastos}>Ver Gastos</button>
         <button onClick={() => limparGastos()}>Limpar Gastos</button>
       </div>
-    </section>
+    </div>
+  </section>
+  
   );
 }
 
