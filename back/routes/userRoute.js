05b6d3357e04581842588053925f880ce41158c2
HipnosM <hipnosmiller@gmail.com>
2025-03-27 14:23:50 -0300

    :sparkles: feat: criados os métodos e rotas para pegar e editar os dados do usuário

--- back/routes/userRoute.js
+++ back/routes/userRoute.js
@@ -1,10 +1,17 @@
 import express from "express";
 import * as userController from "../controllers/userController.js";
+import { auth } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
+// Get userInfo
+router.get("/info", auth, userController.userInfo);
+
 // Cria um novo usuário
 router.post("/registro", userController.newUser);
+
+// Atualiza um usuário
+router.put("/atualizar", auth, userController.updateUser);
 
 // Fazer login
 router.post("/login", userController.login);
